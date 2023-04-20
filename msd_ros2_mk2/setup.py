dcf07486794a13902e261724e7980d9eecb44e7d
sugimoto <tomoki.sugimoto.robot@gmail.com>
2023-04-20 18:24:46 +0900

    create msd_ros2_mk2 package
    create main_motor.py for setup environment

---
+++ msd_ros2_mk2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'msd_ros2_mk2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sugimoto',
+    maintainer_email='tomoki.sugimoto.robot@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'main_motor = msd_ros2_mk2.main_motor:main',
+        ],
+    },
+)
